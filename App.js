b9b41d245bbd3b9a1e1b79b3ed95b2ca3a4d8e33
Yechan0903 Shin <oolo9876@naver.com>
2023-12-13 11:02:00 +0000

    end

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native';
 import * as Location from 'expo-location';
+import {Ionicons, Fontisto} from '@expo/vector-icons';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 
 const {width:SCREEN_WIDTH} = Dimensions.get('window');
 
 const API_KEY = "e48c9cedd3b904ce3d80f88a33babc08"
+
+const icons = {
+  Clouds: "cloudy",
+  Snow: "snow",
+  Clear:"day-sunny",
+}
 
 export default function App() {
   const [city,setCity] = useState("Loading...");
@@ -46,8 +54,12 @@ export default function App() {
         ) :(
           days.map((day, index) =>
           <View key={index} style={styles.day}>
-            <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+            <View style={styles.tempIcon}>
+              <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+              <Fontisto style={styles.icon} name={icons[day.weather[0].main]} size={68} color="white" />
+            </View>
             <Text style={styles.description}>{day.weather[0].main}</Text>
+            <Text style={styles.today}>{day.dt_txt.slice(0,10)}</Text>
           </View>
           )
         )}
@@ -62,27 +74,49 @@ const styles = StyleSheet.create({
     backgroundColor:"tomato"
   },
   city:{
-    flex:1,
+    flex:1.7,
     justifyContent:"center",
     alignItems:"center"
   },
   cityName:{
-    fontSize: 68,
-    fontWeight: "500",
+    fontSize: 60,
+    fontWeight: "900",
+    color:"white",
   },
   weather:{
   },
   day:{
     width: SCREEN_WIDTH,
-    alignItems:"center"
+    alignItems:"flex-start",
+    marginTop:-60,
+  },
+  tempIcon:{
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-between"
   },
   temp: {
     marginTop: 50,
-    fontSize: 150,
+    fontSize: 120,
+    color:"white",
+    marginLeft: 30,
     fontWeight: "600",
   },
+  icon:{
+    marginTop:20,
+    marginLeft:10,
+  },
   description: {
-    marginTop: -30,
-    fontSize: 60,
+    marginTop: -10,
+    fontSize: 50,
+    color:"white",
+    marginLeft: 30,
+    fontWeight: "600",
+  },
+  today:{
+    color:"white",
+    fontSize: 20,
+    marginLeft: 30,
+    fontWeight: "900",
   }
 })
